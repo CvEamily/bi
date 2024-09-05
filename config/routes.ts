3932b97f3c48bab9df7bcff347d16b65fc5af7d5
zmq <2971755387@qq.com>
2024-09-05 11:59:53 +0800

    bi

---
+++ config/routes.ts
@@ -0,0 +1,17 @@
+﻿export default [
+  { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
+  { path: '/', redirect: '/add_chart_async' },
+  { path: '/add_chart_async', name: 'AI智能分析数据', icon: 'barChart', component: './AddChartAsync' },
+  { path: '/my_chart', name: '我的图表', icon: 'pieChart', component: './MyChart' },
+  {
+    path: '/admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', name: '管理页面', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', name: '管理页面2', component: './Admin' },
+    ],
+  },
+  { path: '/', redirect: '/welcome' },
+  { path: '*', layout: false, component: './404' },
+];
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+import React from 'react';
+const Footer: React.FC = () => {
+  return (
+    <DefaultFooter
+      style={{
+        backgroundColor:'pink'
+      }}
+    />
+  );
+};
+export default Footer;
